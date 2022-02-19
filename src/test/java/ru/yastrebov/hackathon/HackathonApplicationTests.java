6d4ef9d843fc3f7f682540d58229bdf2f2104fea
yastrebow <yastrebow@gmail.com>
2022-02-20 02:23:07 +0300

    skip tests

--- src/test/java/ru/yastrebov/hackathon/HackathonApplicationTests.java
+++ src/test/java/ru/yastrebov/hackathon/HackathonApplicationTests.java
@@ -2,7 +2,9 @@ package ru.yastrebov.hackathon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = {"default"})
 @SpringBootTest
 class HackathonApplicationTests {
 
