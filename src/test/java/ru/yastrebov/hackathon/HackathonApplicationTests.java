052bbe09c3b2e331d3376a5f3e519c355dde9034
yastrebow <yastrebow@gmail.com>
2022-02-20 02:26:01 +0300

    clean up

--- src/test/java/ru/yastrebov/hackathon/HackathonApplicationTests.java
+++ src/test/java/ru/yastrebov/hackathon/HackathonApplicationTests.java
@@ -2,9 +2,7 @@ package ru.yastrebov.hackathon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles(profiles = {"default"})
 @SpringBootTest
 class HackathonApplicationTests {
 
